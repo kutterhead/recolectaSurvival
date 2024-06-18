7007ce0956019fcdfe2c63b5cff93e822c82b463
Kutterhead <esseanx@gmail.com>
2024-06-18 20:35:30 +0200

    Se puede fabricar cuchillo y se añade a la lista de objetos.

--- Assets/elementoTienda.cs
+++ Assets/elementoTienda.cs
@@ -1,19 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class elementoTienda : MonoBehaviour
 {
     public gameManager manager;
     public int indiceTipoObjeto;//describe el tipo de objeto
-
-
-
-    void Start()
-    {
-        //por si acaso
-        manager = GameObject.FindGameObjectWithTag("gameManager").GetComponent<gameManager>();
-    }
 
 
     //es el coste de este artículo
@@ -23,19 +16,82 @@ public class elementoTienda : MonoBehaviour
     public int IronValue;
     public int WoodValue;
 
+    public Text goldText;
+    public Text ironText;
+    public Text woodText;
+
+    void Start()
+    {
+        //por si acaso
+        manager = GameObject.FindGameObjectWithTag("gameManager").GetComponent<gameManager>();
+
+        goldText.text = GoldValue.ToString();
+        ironText.text = IronValue.ToString();
+        woodText.text = WoodValue.ToString();
+    }
+
+
+
     public void compraObjeto()
     {
-        int TotalGold = manager.cantidadObjetosConseguidos[2];
-        int TotalWood = manager.cantidadObjetosConseguidos[3];
-        int TotalIron = manager.cantidadObjetosConseguidos[4];
+        int TotalGold = manager.cantidadObjetosConseguidos[2];//oro
+        int TotalWood = manager.cantidadObjetosConseguidos[3];//madera
+        int TotalIron = manager.cantidadObjetosConseguidos[4];//Hierro
+
+        //print("valores totales:");
+        //print(TotalGold + " " + TotalWood + " " + TotalIron);
 
 
-        print("oro total: " + TotalGold);
-        print("madera total: " + TotalWood);
-        print("hierro total: " + TotalIron);
+        //if (TotalGold >= GoldValue)
+        //{
+        //    print("Si puedes G:");
+        //}
+        //else
+        //{
+        //    print("no puedes G:");
+        //}
+        //if (TotalWood >= WoodValue)
+        //{
+        //    print("Si puedes W:");
+        //}
+        //else
+        //{
+        //    print("no puedes W:");
+        //}
+        //if (TotalIron >= IronValue)
+        //{
+        //    print("Si puedes I:");
+        //}
+        //else
+        //{
+        //    print("no puedes I:" + TotalIron + " " + IronValue);
+        //}
 
 
-        print("objeto Comprado");
+        if ((TotalGold>= GoldValue) && (TotalWood>= WoodValue) && (TotalIron>= IronValue))
+        {
+            TotalGold -= GoldValue;
+            TotalWood -= WoodValue;
+            TotalIron -= IronValue;
+
+            print("oro total: " + TotalGold);
+            print("madera total: " + TotalWood);
+            print("hierro ttal: " + TotalIron);
+            manager.cantidadObjetosConseguidos[2] -= GoldValue;//oro
+            manager.cantidadObjetosConseguidos[3] -= WoodValue;//madera
+            manager.cantidadObjetosConseguidos[4] -= IronValue;//Hierro
+            manager.incorporaObjetoEnIndice(indiceTipoObjeto);
+
+
+            print("objeto Comprado");
+
+        }
+        else
+        {
+            print("No se puede realizar la fabricación faltan recursos");
+        }
+
+        
 
 
 
--- Assets/gameManager.cs
+++ Assets/gameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class gameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Sprite[] imagenesIconos;//ejemplos de iconos
+    public bool mostrarCeros;//variable usada para mostrar los iconos que estén a cero o no
 
     public Transform punteroItems;
     Vector3 posicionInicialPuntero;
@@ -36,6 +38,7 @@ public class gameManager : MonoBehaviour
     void Start()
     {
         posicionInicialPuntero = punteroItems.position;
+        ubicaObjetosUI();
     }
 
     // Update is called once per frame
@@ -61,39 +64,55 @@ public class gameManager : MonoBehaviour
         //se ubican de nuevo
             for (int i = 0; i < cantidadObjetosConseguidos.Length; i++)
         {
+            //este if ahhce que se muestren los que son mayor a cero únicamente
 
-            if (cantidadObjetosConseguidos[i]>0)
+
+            if (!mostrarCeros)
             {
+                if (cantidadObjetosConseguidos[i] > 0)
+                {
 
+                ubicar(i);
 
-            
-
-            System.Array.Resize(ref elementosUIEnPantalla, elementosUIEnPantalla.Length + 1);
-
-            //aquí se instancia un icono en el canvas
-            GameObject prefab = Instantiate(itemUIPrefab, punteroItems.position, punteroItems.rotation);
-
-            //lo pone bien "fromateado"
-            prefab.transform.parent = punteroItems.parent;
-            prefab.GetComponent<RectTransform>().sizeDelta = new Vector2(punteroItems.GetComponent<RectTransform>().sizeDelta.x, punteroItems.GetComponent<RectTransform>().sizeDelta.y);
-            prefab.transform.localScale = Vector3.one;
-
-
-            //se almacenan agrupados por tipo aqui
-            elementosUIEnPantalla[elementosUIEnPantalla.Length - 1] = prefab;
-
-            prefab.GetComponent<elementoUIitem>().activaItem(i, cantidadObjetosConseguidos[i]);
-
-
-            float deltaY = punteroItems.GetComponent<RectTransform>().sizeDelta.y/2;
-            punteroItems.transform.position = new Vector3(punteroItems.transform.position.x, punteroItems.transform.position.y - deltaY, punteroItems.transform.position.z);
+                }
             }
+            else
+            {
+                ubicar(i);
+
+            }
+
+
+
+
+
         }
 
         
     }
 
+    private void ubicar(int i)
+    {
+        System.Array.Resize(ref elementosUIEnPantalla, elementosUIEnPantalla.Length + 1);
 
+        //aquí se instancia un icono en el canvas de la izquierda
+        GameObject prefab = Instantiate(itemUIPrefab, punteroItems.position, punteroItems.rotation);
+
+        //lo pone bien "fromateado"
+        prefab.transform.parent = punteroItems.parent;
+        prefab.GetComponent<RectTransform>().sizeDelta = new Vector2(punteroItems.GetComponent<RectTransform>().sizeDelta.x, punteroItems.GetComponent<RectTransform>().sizeDelta.y);
+        prefab.transform.localScale = Vector3.one;
+
+
+        //se almacenan agrupados por tipo aqui
+        elementosUIEnPantalla[elementosUIEnPantalla.Length - 1] = prefab;
+
+        prefab.GetComponent<elementoUIitem>().activaItem(i, cantidadObjetosConseguidos[i]);
+
+
+        float deltaY = punteroItems.GetComponent<RectTransform>().sizeDelta.y / 2;
+        punteroItems.transform.position = new Vector3(punteroItems.transform.position.x, punteroItems.transform.position.y - deltaY, punteroItems.transform.position.z);
+    }
 
     public void pierdeCuchillo()
     {
@@ -110,6 +129,18 @@ public class gameManager : MonoBehaviour
 
         Instantiate(cuchilloPerdidoPrefab, cuchilloEnMano.transform.position, cuchilloEnMano.transform.rotation);
 
+    }
+    public void incorporaObjetoEnIndice(int indice)
+    {
+        if (indice> cantidadObjetosConseguidos.Length-1 && indice <0)
+        {
+            print("Índice incorrecto");
+            return;//no hace nada si el indice es mayor
+        }
+
+        cantidadObjetosConseguidos[indice] += 1;
+
+        ubicaObjetosUI();
     }
 
     public void incorporaObjeto(GameObject objeto)
